607609dc60a0ce09b2373f6cbcf1a3785ec3ee79
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-04-11 10:13:21 +0700

    fix bug

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import { Hono } from 'hono';
+import { serveStatic } from '@hono/node-server/serve-static';
 const app = new Hono();
+
+console.log("cek index.js");
+
 
 // Melayani file statis dari folder public
 app.use('/*', serveStatic({ root: './public' }));
@@ -14,6 +18,6 @@ app.get('/api/hello', (c) => c.json({ message: "API PSB Aktif!" }));
 app.onError((err, c) => {
     console.error(`${err}`);
     return c.json({ error: 'Terjadi kesalahan internal' }, 500);
-})
+});
 
 export default app;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 import apiApp from './api/index.js';
 
+console.log("cek server.js");
+
 const app = new Hono();
 
 // 1. Melayani File Statis (Frontend)
@@ -11,10 +13,12 @@ app.use('/*', serveStatic({ root: './public' }));
 // 2. Gunakan Lokiga API dari api/index.js
 app.route('/', apiApp);
 
-const port = 3000;
-console.log(`Server running on http://localhost:${port}`);
+const port = 3223;
+
 
 serve({
     fetch: app.fetch,
     port
 });
+
+console.log(`Server running on http://localhost:${port}`);
